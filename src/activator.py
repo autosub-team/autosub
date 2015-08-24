1cb9284f7ad83317dd93e67b4a63d6fb21490444
Andreas Platschek <andi.platschek@gmail.com>
2015-08-24 09:27:36 +0200

    activtor puts generation request into gen_queue now
    The taskActivator thread requests puts an message into the gen_queue,
    and the generator script is executed -- for some reason the e-mail
    is not sent yet ...
    
    Signed-off-by: Andreas Platschek <andi.platschek@gmail.com>

--- src/activator.py
+++ src/activator.py
@@ -33,20 +33,56 @@ class taskActivator (threading.Thread):
 
           curc, conc = c.connect_to_db('course.db', self.logger_queue, self.name)
 
-          # first we need to know, for which tasks, the message has already been sent out ... ouch!
+          # first we need to know, for which tasks, the message has already been sent out
           sqlcmd="SELECT * from TaskConfiguration WHERE TaskActive==0;"
           curc.execute(sqlcmd)
           res = curc.fetchone()
           while res != None:
-             logmsg='Task '+str(res[0])+' is still inactive'
+             TaskNr=res[0]
+             logmsg='Task '+str(TaskNr)+' is still inactive'
              c.log_a_msg(self.logger_queue, self.name, logmsg, "INFO")
              # check if a tasks start time has come
              task_starttime = datetime.datetime.strptime(res[1], c.format_string)
-             
-             # check if any users are waiting for that task
+             if task_starttime < datetime.datetime.now():
+                # first, let's set the task active!
+                sqlcmd = "UPDATE TaskConfiguration SET TaskActive=1 WHERE TaskNr=={0}".format(str(TaskNr))
+                curc.execute(sqlcmd)
+                conc.commit()
+ 
+                # next, check if any users are waiting for that task
+                curs, cons = c.connect_to_db('semester.db', self.logger_queue, self.name)
+                sqlcmd = "SELECT * FROM Users WHERE CurrentTask=={0}".format(str(TaskNr))
+                curs.execute(sqlcmd)
+                nextuser = curs.fetchone()
+                while nextuser != None:
+                   logmsg="The next example is sent to User {0} now.".format(str(TaskNr))
+                   c.log_a_msg(self.logger_queue, self.name, logmsg, "INFO")
+                   UserId = nextuser[0]
+                   UserEmail = nextuser[2]
+
+                   try:
+                      sql_cmd="SELECT GeneratorExecutable FROM TaskConfiguration WHERE TaskNr == " + str(TaskNr) + ";"
+                      curc.execute(sql_cmd);
+                      res = curc.fetchone();
+                   except:
+                      logmsg = "Failed to fetch Generator Script for Tasknr: "+ str(TaskNr) 
+                      logmsg = logmsg + "from the Database! Table TaskConfiguration corrupted?"
+                      c.log_a_msg(self.logger_queue, self.name, logmsg, "ERROR")
+
+                   if res != None:
+                      logmsg="Calling Generator Script: " + str(res[0])
+                      c.log_a_msg(self.logger_queue, self.name, logmsg, "DEBUG")
+                      logmsg="UserID " + str(UserId) + ",UserEmail " + str(UserEmail)
+                      c.log_a_msg(self.logger_queue, self.name, logmsg, "DEBUG")
+                      self.gen_queue.put(dict({"UserId": str(UserId), "UserEmail": str(UserEmail), "TaskNr": str(TaskNr), "MessageId": ""}))
+                   else:
+                      c.send_email(self.sender_queue, str(UserEmail), str(UserId), "Task", str(TaskNr), "", "")
+
+                   nextuser = curs.fetchone() 
 
              res = curc.fetchone()
 
+          conc.close() 
 #          time.sleep(3600) # it's more than enough to check every hour!
           time.sleep(6) # for testing purposes only!
 
