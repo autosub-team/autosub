c6c7c852b2aa30295e52f6bc21731c4fd5b66e2f
Andreas Platschek <andreas.platschek@opentech.at>
2016-02-24 21:16:49 +0100

    Added infrastructure to build a .deb package
    Currently only the user package is packed up, and not the code, also the
    dependencies are still missing, etc. -- this really is just a first step
    towards a .deb package.
    
    Signed-off-by: Andreas Platschek <andi.platschek@gmail.com>

---
+++ build_deb.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+if test $# -ne 1;
+then
+    echo "+++ Usage: build_deb.sh <version>"
+    exit 1
+fi
+
+PACKAGE_NAME=autosub-$1
+ORIG_PATH=$(pwd)
+
+export EMAIL="andi.platschek@gmail.com"
+export DEBFULLNAME="Andreas Platschek"
+
+mkdir -p /tmp/$PACKAGE_NAME/bin
+mkdir /tmp/$PACKAGE_NAME/doc
+cd /tmp/$PACKAGE_NAME
+
+git clone https://github.com/andipla/autosub.git
+
+dh_make -y -i --native -c gpl2
+
+#copy prepared config files
+cp $ORIG_PATH/debian/control debian/
+cp $ORIG_PATH/debian/autosub.install debian/
+
+dpkg-buildpackage -uc -us
+
+#clean it all up
+cd ..
+cp autosub_${1}_all.deb $ORIG_PATH
+cp autosub_${1}.tar.xz $ORIG_PATH
+
+cd $ORIG_PATH
+rm -rf /tmp/autosub-$1
