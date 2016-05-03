90d88ffbf89ee943f48b511282c6fcf0cda5b9b4
MartinMosbeck <martin.mosbeck@gmx.at>
2016-05-03 10:40:06 +0200

    Added database_connection coding standard, adapted common.py
    Signed-off-by: MartinMosbeck <martin.mosbeck@gmx.at>

--- src/common.py
+++ src/common.py
@@ -9,7 +9,7 @@
 import sqlite3 as lite
 import os
 import datetime
-   
+
 format_string='%Y-%m-%d %H:%M:%S'
 
 ####
@@ -23,7 +23,7 @@ def log_a_msg(lqueue, lname, msg, loglevel):
 #
 #   returns 0 if the directory already existed, 1 if it had to be created.
 ####
-def check_dir_mkdir(directory, lqueue, lname): 
+def check_dir_mkdir(directory, lqueue, lname):
    if not os.path.exists(directory):
       os.mkdir(directory)
       logmsg = "Created directory: " + directory
@@ -53,37 +53,48 @@ def connect_to_db(dbname, lqueue, lname):
       return -1, -1
 
    cur = con.cursor()
+
    return cur, con
 
 ####
 # increment_db_statcounter()
 ####
-def increment_db_statcounter(cur, con, countername):
-   sql_cmd = "UPDATE StatCounters SET Value=(SELECT Value FROM StatCounters WHERE Name=='" + countername + "')+1 WHERE Name=='" + countername + "';"
-   cur.execute(sql_cmd)
-   con.commit();
+def increment_db_statcounter(semesterdb, countername, lqueue, lname):
+   curs, cons = connect_to_db(semesterdb, lqueue, lname)
+
+   data = {'Name': countername}
+   sql_cmd = "UPDATE StatCounters SET Value = (SELECT Value FROM StatCounters WHERE Name == :Name)+1 WHERE Name == :Name"
+   curs.execute(sql_cmd, data)
+   cons.commit();
+
+   cons.close()
 
 
 def get_task_starttime(coursedb, tasknr, lqueue, lname):
    curc, conc = connect_to_db(coursedb, lqueue, lname)
 
    try:
-      sqlcmd = "SELECT TaskStart FROM TaskConfiguration WHERE TaskNr == '"+ str(tasknr) +"'"
-      curc.execute(sqlcmd)
+      data = {'TaskNr': tasknr}
+      sql_cmd = "SELECT TaskStart FROM TaskConfiguration WHERE TaskNr == :TaskNr"
+      curc.execute(sql_cmd, data)
       tstart_string = str(curc.fetchone()[0])
       ret = datetime.datetime.strptime(tstart_string, format_string)
    except:
       ret = datetime.datetime(1970,1,1,0,0,0)
 
    conc.close()
+
    return ret
 
 
 def get_task_deadline(coursedb, tasknr, lqueue, lname):
    curc, conc = connect_to_db(coursedb, lqueue, lname)
-   sqlcmd = "SELECT TaskDeadline FROM TaskConfiguration WHERE TaskNr == '"+ str(tasknr) +"'"
-   curc.execute(sqlcmd)
+
+   data = {'TaskNr': tasknr}
+   sqlcmd = "SELECT TaskDeadline FROM TaskConfiguration WHERE TaskNr == :TaskNr"
+   curc.execute(sqlcmd, data)
    deadline_string = str(curc.fetchone()[0])
+
    conc.close()
 
    #format_string='%Y-%m-%d %H:%M:%S'
@@ -94,32 +105,45 @@ def get_task_deadline(coursedb, tasknr, lqueue, lname):
 #
 # Set the currentTask of the user with userid to tasknr.
 ####
-def user_set_currentTask(cur, con, tasknr, userid):
-   sql_cmd = "UPDATE Users SET CurrentTask='" + str(tasknr) + "' where UserId=='" + str(userid) + "';"
-   cur.execute(sql_cmd)
-   con.commit();
+def user_set_currentTask(semesterdb, tasknr, userid, lqueue, lname):
+   curs,cons = connect_to_db(semesterdb, lqueue, lname)
+
+   data = {'TaskNr': tasknr, 'UserId': userid}
+   sql_cmd = "UPDATE Users SET CurrentTask = :TaskNr WHERE UserId == :UserId"
+   curs.execute(sql_cmd, data)
+   cons.commit();
+
+   cons.close()
+
 
 ####
 # user_get_currentTask()
 #
 # Get the surrentTask of the user with userid.
 ####
-def user_get_currentTask(cur, con, userid):
-   sql_cmd = "Select CurrentTask from Users where UserId=='" + str(userid) + "';"
-   cur.execute(sql_cmd)
-   res = cur.fetchone();
+def user_get_currentTask(semesterdb, userid, lqueue, lname):
+   curs,cons = connect_to_db(semesterdb, lqueue, lname)
+
+   data = {'UserId': userid}
+   sql_cmd = "SELECT CurrentTask FROM Users WHERE UserId == :UserId"
+   curs.execute(sql_cmd, data)
+   res = curs.fetchone();
+
+   cons.close()
+
    return str(res[0]);
 
 
 ####
 # get_numTasks()
 ####
-def get_num_Tasks(coursedb, lqueue, name):
-   curc, conc = connect_to_db(coursedb, lqueue, name)
-   sqlcmd = "SELECT Content FROM GeneralConfig WHERE ConfigItem == 'num_tasks'"
-   curc.execute(sqlcmd)
+def get_num_Tasks(coursedb, lqueue, lname):
+   curc, conc = connect_to_db(coursedb, lqueue, lname)
+
+   sql_cmd = "SELECT Content FROM GeneralConfig WHERE ConfigItem == 'num_tasks'"
+   curc.execute(sql_cmd)
    numTasks = int(curc.fetchone()[0])
+
    conc.close()
 
    return numTasks
-
