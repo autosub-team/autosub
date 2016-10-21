87303f34954199b3d5cec300afbf047a8daf5c37
Gilbert Markum <gilbert@localhost.localdomain>
2016-10-21 19:38:20 +0200

    allowed having the register file preinitialised with any data; fix for register file size smaller than the size of register 0

--- src/tasks/implementation/VHDL/register_file/templates/testbench_template.vhdl
+++ src/tasks/implementation/VHDL/register_file/templates/testbench_template.vhdl
@@ -97,44 +97,17 @@ begin
    
    begin
    
-   ---------------------------------
-   -- Check normal register bank: --
-   ---------------------------------
+   --------------------------------------
+   -- Check general-purpose registers: --
+   --------------------------------------
    WE2 <= '0';
-   
-      -- write without enabling write:
-      WE1 <= '0';
-      for i in address_test_array'range loop
-         IN1 <= data_n_test_array(i);
-         WA1 <= address_test_array(i)((address_width_n - 1) downto 0);
-         
-         wait for CLK_period;
-         
-      end loop;
-      report "Writing n bit first test data... done";
-   
-   
-      -- read from writes which were not enabled:
-      WE1 <= '0';
-      for i in address_test_array'range loop
-         RA1 <= address_test_array(i)((address_width_n - 1) downto 0);
-         
-         wait for CLK_period;
-         
-         if Output /= data_n_test_array(i) then  -- there should be no senseful data
-               report "good";
-            else
-               report "Data was written although the WE1 bit was not set." severity failure;
-            end if;
-         
-      end loop;
-   
-   
+
       -- write:
       WE1 <= '1';
-      for i in address_test_array'range loop
+      for i in 0 to (N_n - 1) loop
          IN1 <= data_n_test_array(i);
          WA1 <= address_test_array(i)((address_width_n - 1) downto 0);
+         RA1 <= address_test_array(N_n)((address_width_n - 1) downto 0);
          
          if i = N_n - 1 then  -- avoid writing to currently output register address
             RA1 <= (others => '0');
@@ -143,12 +116,12 @@ begin
          wait for CLK_period;
          
       end loop;
-      report "Writing n bit second test data... done";
+      report "Writing n bit first test data... done";
    
    
       -- read:
       WE1 <= '0';
-      for i in address_test_array'range loop
+      for i in 0 to (N_n - 1) loop
          RA1 <= address_test_array(i)((address_width_n - 1) downto 0);
          
          wait for CLK_period;
@@ -161,10 +134,38 @@ begin
             end if;
          else
             if Output = data_n_test_array(i) then
-                  report "good";
-               else
-                  report "The data written to the registers did not match the read data." severity failure;
+               report "good";
+            else
+               report "The data written to the registers did not match the read data." severity failure;
             end if;
+         end if;
+         
+      end loop;
+      
+      
+      -- write without enabling write:
+      WE1 <= '0';
+      for i in 0 to (N_n - 1) loop
+         IN1 <= data_n_test_array(i + N_n);
+         WA1 <= address_test_array(i)((address_width_n - 1) downto 0);
+         
+         wait for CLK_period;
+         
+      end loop;
+      report "Writing n bit second test data... done";
+   
+   
+      -- read from writes which were not enabled:
+      WE1 <= '0';
+      for i in 0 to (N_n - 1) loop
+         RA1 <= address_test_array(i)((address_width_n - 1) downto 0);
+         
+         wait for CLK_period;
+         
+         if Output /= data_n_test_array(i + N_n) then  -- there should be no new data
+            report "good";
+         else
+            report "Data was written although the WE1 bit was not set." severity failure;
          end if;
          
       end loop;
@@ -172,50 +173,50 @@ begin
    
       -- write & read simultaneously
       WE1 <= '1';
-      for i in address_test_array'range loop
+      for i in 0 to (N_n - 1) loop
          IN1 <= data_n_test_array(i + N_n);
          WA1 <= address_test_array(i)((address_width_n - 1) downto 0);
          RA1 <= address_test_array(i)((address_width_n - 1) downto 0);
          
          wait for CLK_period;
          
-    if n_bypass_or_read_priority = '0' then    -- bypass check
-        if i = 0 then
-            if Output /= data_n_test_array(i + N_n) then  -- there should be no senseful data in register 0 for now
-            report "good";
+         if n_bypass_or_read_priority = '0' then    -- bypass check
+            if i = 0 then
+               if Output /= data_n_test_array(i + N_n) then  -- there should be no senseful data in register 0 for now
+                  report "good";
+               else
+                  report "Data was written to register 0 using IN1 as input. This should not be possible." severity failure;
+               end if;
             else
-            report "Data was written to register 0 using IN1 as input. This should not be possible." severity failure;
+               if Output = data_n_test_array(i + N_n) then
+                  report "good";
+               else
+                  report "On a simultaneous read and write from the same register, the input IN1 was not bypassed immediately to the Output." severity failure;
+               end if;
             end if;
-        else
-            if Output = data_n_test_array(i + N_n) then
-                report "good";
+         elsif n_bypass_or_read_priority = '1' then    -- read priority check
+            if i = 0 then
+               if Output /= data_n_test_array(i) then  -- there should be no senseful data in register 0 for now
+                  report "good";
+               else
+                  report "Data was written to register 0 using IN1 as input. This should not be possible." severity failure;
+               end if;
             else
-                report "On a simultaneous read and write from the same register, the input IN1 was not bypassed immediately to the Output." severity failure;
+               if Output = data_n_test_array(i) then
+                  report "good";
+               else
+                  report "On a simultaneous read and write from the same register, the input IN1 was not passed through at the next rising edge of the CLK signal to the Output." severity failure;
+               end if;
             end if;
-        end if;
-    elsif n_bypass_or_read_priority = '1' then    -- read priority check
-        if i = 0 then
-            if Output /= data_n_test_array(i) then  -- there should be no senseful data in register 0 for now
-            report "good";
-            else
-            report "Data was written to register 0 using IN1 as input. This should not be possible." severity failure;
-            end if;
-        else
-            if Output = data_n_test_array(i) then
-                report "good";
-            else
-                report "On a simultaneous read and write from the same register, the input IN1 was not passed through at the next rising edge of the CLK signal to the Output." severity failure;
-            end if;
-        end if;
-    end if;
-          
+         end if;
+            
       end loop;
       WE1 <= '0';
       
       
       -- read again:
       WE1 <= '0';
-      for i in address_test_array'range loop
+      for i in 0 to (N_n - 1) loop
          RA1 <= address_test_array(i)((address_width_n - 1) downto 0);
          
          wait for CLK_period;
@@ -228,9 +229,9 @@ begin
             end if;
          else
             if Output = data_n_test_array(i + N_n) then
-                  report "good";
-               else
-                  report "After a simultaneous read and write from the same register, the written data could not be read out." severity failure;
+               report "good";
+            else
+               report "After a simultaneous read and write from the same register, the written data could not be read out." severity failure;
             end if;
          end if;
          
@@ -239,100 +240,102 @@ begin
    ------------------------------
    -- Check special register 0 --
    ------------------------------
+   WE1 <= '0';
    
-   -- write without enabling write:
-   WE2 <= '0';
-   for i in 0 to (special_reg0_size - 1) loop
-      IN2 <= reg0_test_vector_1(i);
-      WA2 <= address_test_array(i)((address_width_reg0 - 1) downto 0);
+      -- write:
+      WE2 <= '1';
+      for i in 0 to (special_reg0_size - 1) loop
+         IN2 <= reg0_test_vector_1(i);
+         WA2 <= address_test_array(i)((address_width_reg0 - 1) downto 0);
+         
+         wait for CLK_period;
+         
+      end loop;
+      report "Writing reg 0 first test data... done";
       
+      
+      -- read:
+      WE2 <= '0';
+      RA1 <= (others => '0');
+         
       wait for CLK_period;
-      
-   end loop;
-   report "Writing reg 0 first test data... done";
-
-   
-   -- read from writes which were not enabled:
-   WE2 <= '0';
-   
-   RA1 <= (others => '0');
-   
-   wait for CLK_period;
-   
-   if Output((special_reg0_size - 1) downto 0) /= reg0_test_vector_1 then  -- there should be no senseful data
+         
+      if Output((special_reg0_size - 1) downto 0) = reg0_test_vector_1 then
          report "good";
       else
-         report "Data was written although the WE2 bit was not set." severity failure;
-   end if;
-   
-   RA1 <= std_logic_vector(to_unsigned(1, RA1'length)); -- set read address not to 0 so that the following write test is no read & write test
-   
-   
-   -- write:
-   WE2 <= '1';
-   for i in 0 to (special_reg0_size - 1) loop
-      IN2 <= reg0_test_vector_1(i);
-      WA2 <= address_test_array(i)((address_width_reg0 - 1) downto 0);
-      
-     wait for CLK_period;
-      
-   end loop;
-   report "Writing reg 0 second test data... done";
-   
-   
-   -- read:
-   WE2 <= '0';
-   RA1 <= (others => '0');
-      
-      wait for CLK_period;
-      
-      if Output((special_reg0_size - 1) downto 0) = reg0_test_vector_1 then
-            report "good";
-         else
-            report "The data written to the special register 0 did not match the read out data." severity failure;
+         report "The data written to the special register 0 did not match the read out data." severity failure;
       end if;
+      
+      
+      RA1 <= std_logic_vector(to_unsigned(1, RA1'length)); -- set read address not to 0 so that the following write test is no read & write test  
    
    
-   -- write & read simultaneously:
-   WE2 <= '1';
-   for i in 0 to (special_reg0_size - 1) loop
-      IN2 <= reg0_test_vector_2(i); -- "+special_reg0_size" to use the unique second half of the test data
-      WA2 <= address_test_array(i)((address_width_reg0-1) downto 0);
+      -- write without enabling write:
+      WE2 <= '0';
+      for i in 0 to (special_reg0_size - 1) loop
+         IN2 <= reg0_test_vector_2(i);
+         WA2 <= address_test_array(i)((address_width_reg0 - 1) downto 0);
+         
+         wait for CLK_period;
+         
+      end loop;
+      report "Writing reg 0 second test data... done";
+
+      
+      -- read from writes which were not enabled:
+      WE2 <= '0';
+      
       RA1 <= (others => '0');
       
       wait for CLK_period;
       
-      if reg0_bypass_or_read_priority = '0' then    -- bypass check
-        if Output(i) = reg0_test_vector_2(i) then
-            report "good";
-            else
-            report "On a simultaneous read and write from the special register 0, the input IN2 was not bypassed immediately to the Output." severity failure;
-        end if;
-    elsif reg0_bypass_or_read_priority= '1' then    -- read priority check
-    if Output(i) = reg0_test_vector_1(i) then
-            report "good";
-            else
-            report "On a simultaneous read and write from the special register 0, the input IN2 was not passed through at the next rising edge of the CLK signal to the Output." severity failure;
-        end if;
-    end if;
-   
-   end loop;
-   
-   
-   -- read again:
-   WE2 <= '0';
-   RA1 <= (others => '0');
+      if Output((special_reg0_size - 1) downto 0) /= reg0_test_vector_2 then  -- there should be no new data
+         report "good";
+      else
+         report "Data was written to register 0 although the WE2 bit was not set." severity failure;
+      end if;  
       
+      
+      -- write & read simultaneously:
+      WE2 <= '1';
+      for i in 0 to (special_reg0_size - 1) loop
+         IN2 <= reg0_test_vector_2(i); -- "+special_reg0_size" to use the unique second half of the test data
+         WA2 <= address_test_array(i)((address_width_reg0-1) downto 0);
+         RA1 <= (others => '0');
+         
+         wait for CLK_period;
+         
+         if reg0_bypass_or_read_priority = '0' then    -- bypass check
+            if Output(i) = reg0_test_vector_2(i) then
+               report "good";
+            else
+               report "On a simultaneous read and write from the special register 0, the input IN2 was not bypassed immediately to the Output." severity failure;
+            end if;
+         elsif reg0_bypass_or_read_priority= '1' then    -- read priority check
+            if Output(i) = reg0_test_vector_1(i) then
+               report "good";
+            else
+               report "On a simultaneous read and write from the special register 0, the input IN2 was not passed through at the next rising edge of the CLK signal to the Output." severity failure;
+            end if;
+         end if;
+      
+      end loop;
+      
+      
+      -- read again:
+      WE2 <= '0';
+      RA1 <= (others => '0');
+         
       wait for CLK_period;
       
       if Output((special_reg0_size - 1) downto 0) = reg0_test_vector_2 then
-            report "good";
-         else
-            report "After a simultaneous read and write from special register 0, the written data could not be read out again." severity failure;
+         report "good";
+      else
+         report "After a simultaneous read and write from special register 0, the written data could not be read out again." severity failure;
       end if;
-   
-   
-   
+      
+      
+      
       report "Success" severity failure;
    end process test_register_bank_beh;
 
