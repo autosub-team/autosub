f5fb898932536526df720b513674190fb6aeb88c
MartinMosbeck <martin.mosbeck@gmx.at>
2016-11-06 22:21:52 +0100

    Fixed a bug in pwm, comma numbers for frequency now okay!
    How did I never notice this....

--- src/tasks/implementation/VHDL/pwm/scripts/generateTask.py
+++ src/tasks/implementation/VHDL/pwm/scripts/generateTask.py
@@ -2,7 +2,7 @@
 
 ########################################################################
 # generateTask.py for VHDL task pwm
-# Generates random tasks, generates TaskParameters, fill 
+# Generates random tasks, generates TaskParameters, fill
 # entity and description templates
 #
 # Copyright (C) 2015 Martin  Mosbeck   <martin.mosbeck@gmx.at>
@@ -32,9 +32,9 @@ paramsDesc={}
 exp=random.randrange(2,5)
 digit_possibles={0:1,1:2,2:5,3:25}
 digit=digit_possibles[random.randrange(0,4)]
-periodClks=digit*10**exp
+periodClks= 20000
 
-#duty between 10 and 90%    
+#duty between 10 and 90%
 duty=random.randrange(10,91)
 dutyClks=int(periodClks *duty/100)
 
@@ -48,7 +48,7 @@ frequency=10**9/period #in Hz
 #combine the clock counts
 # period_duty#
 # period is max 25000 ->ceil(ld(25000))=18 bit -->use 36 Bit
-taskParameters= (periodClks<<18)+ dutyClks 
+taskParameters= (periodClks<<18)+ dutyClks
 
 
 ############### ONLY FOR TESTING #######################
@@ -60,7 +60,7 @@ with open (filename, "w") as solution:
 #########################################################
 
 ################################################
-# GENERATE PARAMETERS FOR DESCRIPTION TEMPLATE # 
+# GENERATE PARAMETERS FOR DESCRIPTION TEMPLATE #
 ################################################
 unitPeriod={0:" ns",1:" $\\mu$s",2:" ms"}
 unitFrequency={0:" Hz",1:" kHz",2:" MHz"}
@@ -83,12 +83,12 @@ for i in range(0,4):
        break;
 
 #add the prefixes, convert to string
-pwmFrequency=str(int(frequency))+unitFrequency[expFrequency]
-pwmPeriod=str(int(period))+unitPeriod[expPeriod]
+pwmFrequency=str(frequency)+unitFrequency[expFrequency]
+pwmPeriod=str(period)+unitPeriod[expPeriod]
 pwmDuty=str(duty)+" \\%"
 
 ###########################################
-# SET PARAMETERS FOR DESCRIPTION TEMPLATE # 
+# SET PARAMETERS FOR DESCRIPTION TEMPLATE #
 ###########################################
 # FRQ  Frequency
 # DUTY Duty Cycle
