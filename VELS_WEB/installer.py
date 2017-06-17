ff13a2bbc61291e96c41c61be64f3f5235212dbb
MartinMosbeck <martin.mosbeck@tuwien.ac.at>
2017-06-17 22:34:49 +0200

    Added an reinstall flag to the installer of VELS_WEB.
    With this the VELS_WEB can be reconfigured for changes in the
    config file / new config file.

--- VELS_WEB/installer.py
+++ VELS_WEB/installer.py
@@ -13,6 +13,9 @@ import zipfile
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("autosub_path", help="absoulute path to autosub")
 arg_parser.add_argument("configfile", help="absoulute path to used autosub config file")
+help_msg = ("Reinstalls VELS_WEB, setting symlinks new from the supplied"
+            "autosub path and config file")
+arg_parser.add_argument("--reinstall", action="store_true", help=help_msg, )
 
 args = arg_parser.parse_args()
 
@@ -20,13 +23,13 @@ autosub_path = args.autosub_path
 configfile = args.configfile
 
 #check if valid path
-if(os.path.isdir(autosub_path) == False):
+if  not os.path.isdir(autosub_path):
     print("Your autosub_path is not a valid path: " + autosub_path)
     print("Install Failed")
     sys.exit(1)
 
 #check if config file exists
-if(os.path.isfile(configfile) == False):
+if not os.path.isfile(configfile):
     print("Your config file does not exist: " + configfile)
     print("Install Failed")
     sys.exit(1)
@@ -36,6 +39,24 @@ autosub_path = autosub_path.rstrip('/')
 
 #get path to the VELS_WEB itself
 vels_web_path = os.path.dirname(os.path.realpath(__file__))
+
+#home path of the user
+user_home_path = os.path.expanduser('~')
+
+#in case of reinstall delete all symlinks
+if args.reinstall:
+    linkfiles = [user_home_path + "/web2py/applications/VELS_WEB",
+                 vels_web_path+"/databases/semester.db",
+                 vels_web_path+"/databases/course.db",
+                 vels_web_path+"/static/images/nr_mails_received.png",
+                 vels_web_path+"/static/images/nr_mails_sent.png",
+                 vels_web_path+"/static/images/nr_questions_received.png",
+                 vels_web_path+"/static/images/nr_users.png"]
+
+    for path in linkfiles:
+        if os.path.islink(path):
+            os.remove(path)
+
 
 #get paths to databases from configfile
 config = configparser.ConfigParser()
@@ -58,8 +79,7 @@ except:
 
 #download and place web2py
 
-web2py_url="http://www.web2py.com/examples/static/web2py_src.zip"
-user_home_path = os.path.expanduser('~')
+web2py_url = "http://www.web2py.com/examples/static/web2py_src.zip"
 
 local_file = user_home_path + "/web2py.zip"
 
@@ -76,15 +96,15 @@ print("Unzip complete")
 #Set all the needed symlinks
 
 print("Setting up web2py")
-os.symlink(vels_web_path,user_home_path+"/web2py/applications/VELS_WEB")
+os.symlink(vels_web_path, user_home_path + "/web2py/applications/VELS_WEB")
 
-os.makedirs(vels_web_path+"/databases",exist_ok=True)
-os.symlink(semesterdb_file,vels_web_path+"/databases/semester.db")
-os.symlink(coursedb_file,vels_web_path+"/databases/course.db")
+os.makedirs(vels_web_path + "/databases", exist_ok=True)
+os.symlink(semesterdb_file, vels_web_path + "/databases/semester.db")
+os.symlink(coursedb_file, vels_web_path + "/databases/course.db")
 
-os.symlink(autosub_path+"/src/nr_mails_received.png",vels_web_path+"/static/images/nr_mails_received.png")
-os.symlink(autosub_path+"/src/nr_mails_sent.png",vels_web_path+"/static/images/nr_mails_sent.png")
-os.symlink(autosub_path+"/src/nr_questions_received.png",vels_web_path+"/static/images/nr_questions_received.png")
-os.symlink(autosub_path+"/src/nr_users.png",vels_web_path+"/static/images/nr_users.png")
+os.symlink(autosub_path+"/src/nr_mails_received.png", vels_web_path+"/static/images/nr_mails_received.png")
+os.symlink(autosub_path+"/src/nr_mails_sent.png", vels_web_path+"/static/images/nr_mails_sent.png")
+os.symlink(autosub_path+"/src/nr_questions_received.png", vels_web_path+"/static/images/nr_questions_received.png")
+os.symlink(autosub_path+"/src/nr_users.png", vels_web_path+"/static/images/nr_users.png")
 
 print("Setting up completed")
