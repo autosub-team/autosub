02220d0426dafeba034d4d7111b888bbdab86d8f
MartinMosbeck <martin.mosbeck@tuwien.ac.at>
2017-08-22 13:59:07 +0200

    New solution template

--- tasks/implementation/VHDL/blockcode/solution_template/blockcode_beh.vhdl
+++ tasks/implementation/VHDL/blockcode/solution_template/blockcode_beh.vhdl
@@ -2,13 +2,13 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-architecture behavior of blockcode_source is
+architecture behavior of blockcode is
 	constant fifo_length : integer := 3;
 
-	constant code_width : integer := /*CHANGE*/;
-	constant data_width : integer := /*CHANGE*/;
+	constant code_width : integer := ; --HERE
+	constant data_width : integer := ; --HERE
 
-	type memory_type is array (fifo_length-1 downto 0) of std_logic_vector(code_width-1 downto 0);
+	type memory_type is array (fifo_length-1 downto 0) of std_logic_vector(0 to code_width-1);
 
 	type fifo_state is record
 		data				: memory_type;
@@ -17,11 +17,14 @@ architecture behavior of blockcode_source is
 	end record fifo_state;
 
 	type output_type is record
-		code			: std_logic_vector(code_width-1 downto 0);
+		code			: std_logic_vector(0 to code_width-1);
 		code_valid		: std_logic;
 	end record output_type;
 
 	signal output, output_next : output_type;
+	signal fifo_sig : fifo_state;
+	signal place_sig : boolean;
+	signal code_available_sig : boolean;
 
 begin
 
@@ -30,6 +33,7 @@ begin
 		variable sending : boolean;
 		variable transmission_success : boolean;
 		variable code_available : boolean;
+		variable place_left: boolean;
 	begin
 		if rst = '1' then
 			fifo.data := (others => (others => '0'));
@@ -38,34 +42,36 @@ begin
 			fifo.looped := false;
 
 			output_next.code <= (others => '0');
-            output_next.code_valid <= '0';
+			output_next.code_valid <= '0';
 
 			sending := false;
 			code_available := false;
 			transmission_success := false;
+			place_left := true;
 		else
-			--Prevent Latches
+			
 			output_next	<= output;
 
 			sending := output.code_valid = '1';
 			transmission_success := sending and (sink_ready = '1');
 
-			-- valid data -> encode and buffer
-			if data_valid = '1' and ((fifo.looped = false) or
-			(fifo.write_ptr /= fifo.read_ptr)) then
-				-- encode new data element and buffer it
-				/*CHANGE*/
+			place_left := (fifo.looped = false) or (fifo.write_ptr /= fifo.read_ptr);
+			place_sig <= place_left;
+			
+			if data_valid = '1' and (place_left = true) then    
+			--HERE
 
-				-- advance write pointer
+				
 				if fifo.write_ptr = fifo_length-1 then
 					fifo.write_ptr := 0;
 					fifo.looped := true;
 				else
 					fifo.write_ptr := fifo.write_ptr + 1;
 				end if;
-			end if; -- end valid data
+			end if; 
 
 			code_available := (fifo.looped = true) or (fifo.write_ptr /= fifo.read_ptr);
+			code_available_sig <= code_available;
 
 			if (not sending or transmission_success) and (code_available = true) then
 				output_next.code <= fifo.data(fifo.read_ptr);
@@ -78,11 +84,13 @@ begin
 					fifo.read_ptr := fifo.read_ptr + 1;
 				end if;
 			elsif sending and not transmission_success then
-				-- hold current state
+				
 			else
 				output_next.code_valid <= '0';
-			end if; -- end sink ready
-		end if; -- end if reset
+			end if; 
+		end if; 
+		
+		fifo_sig <= fifo;
 
 	end process encode_buffer;
 
@@ -94,7 +102,7 @@ begin
 
 	end process synch;
 
-	-- Assign internal to output
+	
 	code_valid	<= output.code_valid;
 	code		<= output.code;
 
