2d6988d8802f0314a2bc5b12ec71774821cd9f60
Daniel Hauer <e1026951@student.tuwien.ac.at>
2021-12-23 10:55:34 +0100

    added system status flag and recent errors to VELS_WEB

--- VELS_WEB/controllers/default.py
+++ VELS_WEB/controllers/default.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime,timedelta
 
 def __tail(filename, n=1, bs=1024):
     try:
@@ -18,6 +19,31 @@ def __tail(filename, n=1, bs=1024):
     except:
         return ["Error reading log at " + filename]
 
+def __tail_error(filename, n=1, bs = 1024, key = "ERROR"):
+    try:
+        tail=(__tail(filename,n,bs))
+        error_lines=[]
+        for line in tail:
+            if "ERROR" in line:
+                error_lines.append(line)
+        if(error_lines==[]):
+            error_lines = ["No Errors found in the last {} log entries".format(n)]
+        return error_lines
+    except:
+        return ["Error reading log at " + filename]
+
+def __check_running(filename,polling_period = 30):
+    try:
+        tail=(__tail(filename,1))
+        latest_timestamp = datetime.strptime(tail[0].split(",")[0],"%Y-%m-%d %H:%M:%S")
+        delta_latest_timestamp = datetime.now()-latest_timestamp
+        if(delta_latest_timestamp<timedelta(seconds=polling_period)):
+            return True
+        else:
+            return False
+    except:
+        return False
+
 @auth.requires_login()
 def index():
     row=course(GeneralConfig.ConfigItem =='course_name').select(GeneralConfig.ALL).first()
@@ -26,10 +52,37 @@ def index():
     row=course(GeneralConfig.ConfigItem =='log_dir').select(GeneralConfig.ALL).first()
     log_dir = row.Content
 
+    row=course(GeneralConfig.ConfigItem =='current_config').select(GeneralConfig.ALL).first()
+    config_file = row.Content
+
+    # get polling period from config
+    polling_period = 30
+    open_config_file = open(config_file, "r")
+    for line in open_config_file:
+        if "server_timeout" in line:
+            try:
+                polling_period = int(line.split(":")[1])
+            except:
+                polling_period = 30 
+           
     autosub_log = os.path.join(log_dir, "autosub.log")
     log_lines = __tail(autosub_log,20)
+    
+    # for recent errors approximate interesting number based on polling periode (this is just a guess based on at least 2 entries per polling period)
+    error_lines = __tail_error(autosub_log,int(round((60/polling_period*60*24))/1000+1)*2000)
+    
+    if(__check_running(autosub_log,polling_period+5)):
+        if("No Errors found" in error_lines[0]):
+                vels_status = "Up and running"
+                vels_status_color = "green"
+        else:
+                vels_status = "Up and running with recent errors"
+                vels_status_color = "yellow"
+    else:
+        vels_status = "Down or paused"
+        vels_status_color = "red"
 
-    return_dict ={'course_name':course_name, 'log_lines': log_lines}
+    return_dict ={'course_name':course_name, 'log_lines': log_lines,'error_lines':error_lines,'vels_status':vels_status, 'vels_status_color':vels_status_color}
 
     return return_dict
 
