e3dc9fde85c19fa9f8848b73c2a5f083c07bf090
Philipp-S. Vogt <philipp-sebastian.vogt@tuwien.ac.at>
2023-10-20 08:52:16 +0200

    fix melodi integration

--- VELS_WEB/controllers/vels_plugins.py
+++ VELS_WEB/controllers/vels_plugins.py
@@ -1,3 +1,6 @@
+# Please verify that VELS_OB Swagger client is copied into web2py/site-packages/swagger_client
+import swagger_client as api_vels_ob
+
 def __list_plugins():
 
     #####
@@ -13,10 +16,11 @@ def __list_plugins():
     for row in rows:
         if(row.PluginName not in unique_plugins):
             unique_plugins.append(row.PluginName)
-            current_plugin = []
+            current_plugin = dict({})
             for subrow in rows:
                 if(unique_plugins[-1] == subrow.PluginName):
-                    current_plugin.append(subrow.ParameterName)
+                    current_plugin.update({subrow.ParameterName : subrow.Value})
+                
             parameterDict.update({row.PluginName : current_plugin})
         parameterDict.update({row.PluginName : current_plugin})
 
@@ -25,18 +29,27 @@ def __list_plugins():
 
     return returnDict
 
-def __assemble_plugin(plugin_name):
+def __assemble_plugin(plugin_name, returnDict):
 
     #####
     # set entries for each plugin
     # plugin_active controls if plugin is visible at VELS_WEB
     # plugin_active has to be set for every possible plugin
     #####
-    returnDict={}
 
-    #if(plugin_name == 'vels_ob'):
-     #   returnDict.update({'vels_ob_active': True})
+    if(plugin_name == 'vels_ob'):
+        returnDict.update({'vels_ob_active': True})
+        
+        MelodiURL = returnDict.get("PluginParameter").get(plugin_name).get("server")
+        MelodiPort = returnDict.get("PluginParameter").get(plugin_name).get("port")
+        MelodiPassword = returnDict.get("PluginParameter").get(plugin_name).get("password")
 
+        api_con = api_vels_ob.Configuration()
+        api_con.host = "{}:{}/api".format(MelodiURL, MelodiPort)
+        task_instance = api_vels_ob.TaskApi(api_vels_ob.ApiClient(api_con))
+        all_tasks = task_instance.list_of_tasks()
+        
+        returnDict.update({'vels_ob_tasks': all_tasks})
     return returnDict
 
 
@@ -45,7 +58,7 @@ def index():
     returnDict={}
     returnDict.update(__list_plugins())
     
-    returnDict.update(__assemble_plugin('vels_ob'))
+    __assemble_plugin('vels_ob', returnDict)
     return returnDict
 
 #@auth.requires_permission('edit data')
